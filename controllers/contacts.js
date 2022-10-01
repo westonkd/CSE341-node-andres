2d3b050f87ca5006357250b50272a10633d706a1
Weston Dransfield <wdrans@gmail.com>
2022-10-01 07:46:58 -0600

    update db name to match mine

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,7 @@ const ObjectId = require("mongodb").ObjectId;
 const getAll = async (req, res) => {
   const result = await mongodb
     .getDb()
-    .db("contacts")
+    .db("cs_341_projects")
     .collection("contacts")
     .find();
   result.toArray().then((lists) => {
@@ -17,7 +17,7 @@ const getSingle = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb
     .getDb()
-    .db("contacts")
+    .db("cs_341_projects")
     .collection("contacts")
     .find({ _id: userId });
   result.toArray().then((lists) => {
